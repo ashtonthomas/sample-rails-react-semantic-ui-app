f3d49ed573ee97b15adf7cf4bbfd9e3462758431
Ashton Thomas <ashton@acrinta.com>
2018-01-28 14:04:41 -0600

    get rid of console warnings Get rid of console warnings:
    https://github.com/rails/webpacker/issues/1057
    
    /content_security_policy.rb

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -9,6 +9,7 @@ Rails.application.config.content_security_policy do |p|
   p.object_src  :none
   p.script_src  :self, :https
   p.style_src   :self, :https, :unsafe_inline
+  p.connect_src :self, :https, 'http://localhost:3035', 'ws://localhost:3035' if Rails.env.development?
 
   # Specify URI for violation reports
   # p.report_uri "/csp-violation-report-endpoint"
